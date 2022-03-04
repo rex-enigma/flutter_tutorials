7f4a13221cdd449205ea3cfad82321f4d86a8267
josh <infinityjosh.jn@gmail.com>
2022-03-04 22:43:06 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,54 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final stars = Row(
+    children: <Widget>[
+      Icon(
+        Icons.star,
+        color: Colors.green[500],
+      ),
+      Icon(
+        Icons.star,
+        color: Colors.green[500],
+      ),
+      Icon(
+        Icons.star,
+        color: Colors.green[500],
+      ),
+      Icon(
+        Icons.star,
+        color: Colors.black,
+      ),
+      Icon(
+        Icons.star,
+        color: Colors.black,
+      ),
+    ],
+  );
+
+  Widget rating() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          const Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +63,10 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter layout demo'),
         ),
         body: Center(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: <Widget>[
-              Expanded(child: Image.asset('images/pic1.jpg')),
-              Expanded(
-                flex: 2,
-                child: Image.asset('images/pic2.jpg'),
-              ),
-              Expanded(child: Image.asset('images/pic3.jpg')),
+              rating(),
+              iconList,
             ],
           ),
         ),
@@ -32,3 +74,47 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+const descTextStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.5,
+  fontSize: 18,
+  height: 2,
+);
+
+// DefaultTextStyle.merge() allows you to create a default text
+// style that is inherited by its child and all subsequent children.
+final iconList = DefaultTextStyle.merge(
+  style: descTextStyle,
+  child: Container(
+    padding: const EdgeInsets.all(20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Icon(Icons.kitchen, color: Colors.green[500]),
+            const Text('PREP:'),
+            const Text('25 min'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.timer, color: Colors.green[500]),
+            const Text('COOK:'),
+            const Text('1 hr'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.restaurant, color: Colors.green[500]),
+            const Text('FEEDS:'),
+            const Text('4-6'),
+          ],
+        ),
+      ],
+    ),
+  ),
+);
