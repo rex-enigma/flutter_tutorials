a54b304fc03a566ee67a43fa3c40f93113703efc
josh <infinityjosh.jn@gmail.com>
2022-03-12 09:22:51 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -7,114 +7,58 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  final stars = Row(
-    children: <Widget>[
-      Icon(
-        Icons.star,
-        color: Colors.green[500],
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.green[500],
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.green[500],
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.black,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.black,
-      ),
-    ],
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'kandersteg Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        const Text('41'),
+      ],
+    ),
   );
 
-  Widget rating() {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          stars,
-          const Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+
+  Column _buildButtonColunm({Color? color, Icon? icon, Text text = ''}){}
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
+      title: 'Flutter Layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter layout demo'),
+          title: Text('Flutter Layout demo'),
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              rating(),
-              iconList,
-            ],
-          ),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
   }
 }
-
-const descTextStyle = TextStyle(
-  color: Colors.black,
-  fontWeight: FontWeight.w800,
-  fontFamily: 'Roboto',
-  letterSpacing: 0.5,
-  fontSize: 18,
-  height: 2,
-);
-
-// DefaultTextStyle.merge() allows you to create a default text
-// style that is inherited by its child and all subsequent children.
-final iconList = DefaultTextStyle.merge(
-  style: descTextStyle,
-  child: Container(
-    padding: const EdgeInsets.all(20),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Column(
-          children: [
-            Icon(Icons.kitchen, color: Colors.green[500]),
-            const Text('PREP:'),
-            const Text('25 min'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(Icons.timer, color: Colors.green[500]),
-            const Text('COOK:'),
-            const Text('1 hr'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(Icons.restaurant, color: Colors.green[500]),
-            const Text('FEEDS:'),
-            const Text('4-6'),
-          ],
-        ),
-      ],
-    ),
-  ),
-);
