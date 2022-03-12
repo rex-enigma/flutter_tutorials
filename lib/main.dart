7afe9ea99453e65052f8f083567f1e13a36fda41
rex-enigma <infinityjosh.jn@gmail.com>
2022-03-12 15:15:47 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -42,8 +42,49 @@ class MyApp extends StatelessWidget {
     ),
   );
 
+  Column _buildButtonColumn({required Color color, required IconData icon, required String label}) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              color: color,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
-  Column _buildButtonColunm({Color? color, Icon? icon, Text text = ''}){}
+  late Widget buttonSection = Row(
+    // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color: Colors.blue, icon: Icons.call, label: 'CALL'),
+      _buildButtonColumn(color: Colors.blue, icon: Icons.near_me, label: 'ROUTE'),
+      _buildButtonColumn(color: Colors.blue, icon: Icons.share, label: 'SHARE'),
+    ],
+  );
+
+  Widget textSection = const Padding(
+    padding: EdgeInsets.all(32.0),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+      'Alps. Situated 1,578 meters above sea level, it is one of the '
+      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+      'half-hour walk through pastures and pine forest, leads you to the '
+      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+      'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +96,15 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
